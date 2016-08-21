772d3fda494860a94777a5aa491201b0536a01f4
leafriend <leafriend@gmail.com>
2016-08-22 02:25:59 +0900

    Extract font as field.

--- src/main/java/com/leafriend/clo/PdfGenerator.java
+++ src/main/java/com/leafriend/clo/PdfGenerator.java
@@ -17,7 +17,14 @@ public class PdfGenerator {
 
     private Format format = new Format();
 
+    private Font font;
+
     public void generate() throws DocumentException, IOException {
+
+        BaseFont bf = BaseFont.createFont(
+                "src/main/resources/NotoSansCJKjp-Regular.otf",
+                BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        font = new Font(bf, 10);
 
         String dest = "target/lyrics.pdf";
 
@@ -41,10 +48,6 @@ public class PdfGenerator {
             throws DocumentException, IOException {
 
         String lyrics = "Yeah Yeah 独壇場beauty\n孤独 絶望 Yeah Yeah お前が自由\n\nYeah Yeah 花も灰も beauty\n風も命も Yeah Yeah 無限も永遠も\n\nオーマイガッ\n神様も使えないな それなら勝手にやっちゃえ\nワインも煙草も薔薇もある 俺が笑って見ててやる\n\nYeah Yeah 飛ばしてくれ beauty\nあの空の上 Yeah Yeah お前の自由\n\nYeah Yeah 優しんだな beauty\nもう大丈夫 Yeah Yeah 何も起きないさ\n\nオーマイガッ\n神様は見ないふり それなら派手にやっちゃえ\n死ぬほど楽しめ踊れ 俺が笑って見ててやる\n\nGo Go beauty round round Nothing's gonna stop\nYeah beauty round round Nothing's gonna stop\nGo Go beauty round round Nothing's gonna stop\nYeah beauty round round Nothing's gonna stop Yeah\n\nオーマイガッ\n神様も止められない 思うがままが人生\n最後に全部食べちゃえよ 俺が笑って見ててやる\n\nGo Go beauty round round Nothing's gonna stop\nYeah beauty round round Nothing's gonna stop\nGo Go beauty round round Nothing's gonna stop\nYeah beauty round round Nothing's gonna stop Yeah\n\nGo Go beauty round round Nothing's gonna stop\nYeah beauty round round Nothing's gonna stop\nGo Go beauty round round Nothing's gonna stop\nYeah beauty round round Nothing's gonna stop\nGo Go beauty round round Nothing's gonna stop\nYeah beauty round round Nothing's gonna stop Yeah";
-        BaseFont bf = BaseFont.createFont(
-                "src/main/resources/NotoSansCJKjp-Regular.otf",
-                BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        Font font = new Font(bf, 10);
 
         float[] columnWidths = { format.getLeftWidth(),
                 format.getBetweenWidth(), format.getRightWidth() };
