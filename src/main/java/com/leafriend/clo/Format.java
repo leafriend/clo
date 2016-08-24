ebdb6cad6fc1ed4f16c95438d816f1660064bbff
leafriend <leafriend@gmail.com>
2016-08-24 23:27:15 +0900

    Extract default values to properties file.

--- src/main/java/com/leafriend/clo/Format.java
+++ src/main/java/com/leafriend/clo/Format.java
@@ -1,25 +1,66 @@
 package com.leafriend.clo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
 public class Format {
 
-    private double width_mm = 297;
-    private double height_mm = 210;
-    private double horizontalMargin = 25.4;
-    private double verticalMargin = 19.1;
+    private static final String MM = "mm";
 
-    private float width = mm2pt(width_mm);
-    private float height = mm2pt(height_mm);
+    private final Properties props;
 
-    private float marginLeft = mm2pt(horizontalMargin);
-    private float marginRight = mm2pt(horizontalMargin);
-    private float marginTop = mm2pt(verticalMargin);
-    private float marginBottom = mm2pt(verticalMargin);
+    private final float width;
+    private final float height;
 
-    private float spaceBetween = mm2pt(horizontalMargin);
+    private final float marginLeft;
+    private final float marginRight;
+    private final float marginTop;
+    private final float marginBottom;
 
-    private float leftWidth = mm2pt(width - horizontalMargin * 3) / 2;
-    private float betweenWidth = spaceBetween;
-    private float rightWidth = mm2pt(width - horizontalMargin * 3) / 2;
+    private final float leftWidth;
+    private final float betweenWidth;
+    private final float rightWidth;
+
+    public Format() {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream in = classLoader.getResourceAsStream("clo.properties");
+
+        props = new Properties();
+        try {
+            props.load(in);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw new RuntimeException("Not handled", e);
+        }
+
+        width = loadAsPt("format.paper.width");
+        height = loadAsPt("format.paper.height");
+
+        marginTop = loadAsPt("format.margin.top");
+        marginLeft = loadAsPt("format.margin.left");
+        marginRight = loadAsPt("format.margin.right");
+        marginBottom = loadAsPt("format.margin.bottom");
+
+        betweenWidth = loadAsPt("format.margin.center");
+        leftWidth = (width - (marginLeft + betweenWidth + marginRight)) / 2;
+        rightWidth = (width - (marginLeft + betweenWidth + marginRight)) / 2;
+
+    }
+
+    private float loadAsPt(String key) {
+        String value = props.getProperty(key);
+        if (Pattern.matches("^\\d+(\\.\\d+)?" + MM + "$", value)) {
+            double scalar = Double.parseDouble(
+                    value.substring(0, value.length() - MM.length()));
+            return mm2pt(scalar);
+        } else {
+            throw new RuntimeException("Unsupported unit value: " + value);
+        }
+    }
 
     public static float mm2pt(double mm) {
         return (float) (mm / 25.4 * 72);
