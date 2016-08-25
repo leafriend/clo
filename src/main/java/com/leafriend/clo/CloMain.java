50ddef766c581888addde3d3e5d01365aa37a4e1
leafriend <leafriend@gmail.com>
2016-08-26 00:58:29 +0900

    Travers resource subdirs and gen all lyrics.

--- src/main/java/com/leafriend/clo/CloMain.java
+++ src/main/java/com/leafriend/clo/CloMain.java
@@ -7,27 +7,53 @@ import com.itextpdf.text.DocumentException;
 
 public class CloMain {
 
+    private File txtDir;
+
+    private File pdfDir;
+
     public static void main(String[] args)
             throws DocumentException, IOException {
+        new CloMain("resources/lyrics/txt", "resources/lyrics/pdf")
+                .traverse("");
+    }
 
-        String lyricsPath = "Buck-Tick/[2010-03-24] 独壇場Beauty/01-独壇場Beauty.txt";
+    public CloMain(String txtName, String pdfName) {
+        txtDir = new File(txtName);
+        pdfDir = new File(pdfName);
+    }
 
-        File lyricsDir = new File("resources/lyrics/txt");
-        File lyricsFile = new File(lyricsDir, lyricsPath);
+    public void traverse(String path) throws DocumentException, IOException {
+        File dir = new File(txtDir, path);
+        for (File file : dir.listFiles()) {
+            String subpath = path + "/" + file.getName();
+            if (file.isDirectory()) {
+                traverse(subpath);
+            } else if (file.isFile()) {
+                generate(subpath);
+            }
+        }
 
-        String album = lyricsFile.getParentFile().getName();
-        String title = lyricsFile.getName();
+    }
+
+    public void generate(String txtPath) throws DocumentException, IOException {
+
+        if (!txtPath.endsWith(".txt"))
+            return;
+        System.out.println(txtPath);
+
+        File txtFile = new File(txtDir, txtPath);
+
+        String album = txtFile.getParentFile().getName();
+        String title = txtFile.getName();
         title = title.substring(0, title.lastIndexOf('.'));
 
-        File pdfDir = new File("resources/lyrics/pdf");
-        String pdfPath = lyricsPath.substring(0, lyricsPath.length() - 4)
-                + ".pdf";
+        String pdfPath = txtPath.substring(0, txtPath.length() - 4) + ".pdf";
         File pdfFile = new File(pdfDir, pdfPath);
         pdfFile.getParentFile().mkdirs();
 
-        String lyrics = lyricsFile.getCanonicalPath();
+        String txt = txtFile.getCanonicalPath();
         String pdf = pdfFile.getCanonicalPath();
-        new PdfGenerator().generate(album, title, lyrics, pdf);
+        new PdfGenerator().generate(album, title, txt, pdf);
 
     }
 
